6e0146de6cef9950645340cf34959bd113cff5bb
Leo Policastro <leo.policastro@hey.com>
2022-03-19 07:34:15 -0400

    create comments

--- routes/secure/books.rb
+++ routes/secure/books.rb
@@ -6,8 +6,14 @@ namespace '/books' do
     erb 'books/index'.to_sym, layout: :default
   end
 
-  get '/:id' do
-    @book = Book.find(params[:id])
+  get '/:book_id/' do
+    @book = Book.find(params[:book_id])
     erb 'books/show'.to_sym, layout: :default
+  end
+
+  post '/:book_id/comments' do
+    @book = Book.find(params[:book_id])
+    @book.comments.create(body: params['body'])
+    redirect "/books/#{@book.id}/"
   end
 end
