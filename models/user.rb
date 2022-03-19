a70bb0567887f521ae281a276864b941d08b220f
Leo Policastro <leo.policastro@hey.com>
2022-03-18 20:17:17 -0400

    update indexes

--- models/user.rb
+++ models/user.rb
@@ -14,6 +14,7 @@ class User
   validates :email, presence: true
   validates :password, presence: true
 
-  index({ email: 'text' })
+  index({ email: 1 }, { unique: true })
+
   scope :admin, -> { where(admin: true) }
 end
